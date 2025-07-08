c2cabba7cf36acd6423251e6327553f31c64024a
Nuke <Ca.vasquezq@duocuc.cl>
2025-07-08 18:54:03 -0400

    Agregando una conexión entre el microservicio Usuario y Producto

--- medi-express/src/main/java/com/examplempresas/medi_express/SecurityConfig.java
+++ medi-express/src/main/java/com/examplempresas/medi_express/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -40,5 +41,17 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Configuration
+    public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    }
+
+
+
 
 }
